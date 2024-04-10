d5d74648ee79f5984313f69d843d88dcaeac512a
Mihir Mandavia <mihirmandavia7@gmail.com>
2024-04-10 19:38:02 +0530

    understanding pointers

---
+++ pointers/basic_pointers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a;       //normal integer
+    int* ptra;  // pointer to store the address of a
+    printf("Enter value for a: ");
+    scanf("%d",&a);
+
+    ptra=&a;    //Assigning address of a to ptra
+
+    printf("Address of a: %p\n",&a);
+    printf("Address of a using pointer: %p\n",ptra);
+
+    printf("Address of pointer: %p",&ptra);
+
+    printf("\n\nValue of a: %d",a);
+    printf("\nValue of a using pointer: %d\n",*ptra); //getting value of a using dereferencing operator(*)
+
+    return 0;
+}
